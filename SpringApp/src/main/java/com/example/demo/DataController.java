ed7973fc12ea1331b99d65034a76d6283800d849
serbalta <salih.773@gmail.com>
2025-04-10 23:57:18 +0200

    MSA-se22m061-Salih Erbalta-Project1

--- SpringApp/src/main/java/com/example/demo/DataController.java
+++ SpringApp/src/main/java/com/example/demo/DataController.java
@@ -3,15 +3,30 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 class DataController {
+
+    private AtomicInteger callsCount = new AtomicInteger(0);
+
     @GetMapping("/data")
     SomeData get() {
+        callsCount.incrementAndGet();
         var ret = new SomeData();
         ret.setName("ILV Microservice Architecture");
         return ret;
     }
+
+    @GetMapping(value = "/metrics", produces = "text/plain")
+    String metrics() {
+        return "# HELP calls_count Total number of calls\n" +
+                "# TYPE calls_count counter\n" +
+                "calls_count " + callsCount.get() + "\n";
+    }
 }
+
+
 
 class SomeData {
     public String getName() {
